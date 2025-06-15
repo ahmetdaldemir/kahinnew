4fdbb1ddf663fe2cafe456e99be89b114dfb02c6
Ahmet DALDEMİR <ahmetdaldemirdeveloper@gmail.com>
2025-06-15 22:44:48 +0300

    d

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'kahinnew',
-    script: 'index.js',
+    script: 'server.js',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -11,4 +11,4 @@ module.exports = {
       PORT: 3200
     }
   }]
-}; 
+};
