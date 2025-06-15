92a0d7e3161432ec31c37029c073352b0195d252
Ahmet DALDEMİR <ahmetdaldemirdeveloper@gmail.com>
2025-06-15 22:33:14 +0300

    d

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ function startExpressServer() {
         });
     });
 
-    app.listen(port, () => {
+    app.listen(port,'0.0.0.0', () => {
         console.log(`Server running at http://localhost:${port}`);
     });
 }
